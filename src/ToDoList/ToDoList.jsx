12125a0124a11f69dd990cc7495e1e2904dbf5b0
sggyflt <sgflt88@gmail.com>
2024-09-04 22:36:42 +0200

    feat: add structure to ToDoList component

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 
 function ToDoList() {
+
   return(
-    <>
-    </>
+    <div>
+      <h1>To Do List</h1>
+      <div>
+        <input type="text" placeholder="Write a task..."/>
+        <button>Add</button>
+      </div>
+      <ol>
+        <li>
+          <span>Task 1</span>
+          <button>Delete</button>
+          <button>Move up</button>
+          <button>Move down</button>
+        </li>
+      </ol>
+    </div>
   )
 }
 
