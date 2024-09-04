814e399a2854b8d9b4298f0dbc371370bcd54226
sggyflt <sgflt88@gmail.com>
2024-09-04 23:28:28 +0200

    feat: add new task functionality

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -12,15 +12,20 @@ function ToDoList() {
     setNewTask(event.target.value);
   }
 
+  function addTask() {
+    setTasks(t => [...t, newTask]);
+    setNewTask("");
+  }
+
   return(
     <div className="flex flex-col items-start justify-start w-1/4 h-screen gap-6 m-auto mt-40">
       <h1 className="text-5xl">To Do List</h1>
       <div className="flex gap-1">
         <input className="pl-2 border border-gray-300 rounded-md w-96" type="text" placeholder="Write a task..." value={newTask} onChange={handleInputChange}/>
-        <button className="px-4 py-2 text-white bg-green-600 rounded-md hover:bg-green-700">Add</button>
+        <button className="px-4 py-2 text-white bg-green-600 rounded-md hover:bg-green-700" onClick={addTask}>Add</button>
       </div>
       <ol>
-        {tasks.map((task, index) => <li className="flex items-center gap-1 px-4 py-2 rounded-md bg-slate-200">
+        {tasks.map((task, index) => <li key={index} className="flex items-center gap-1 px-4 py-2 rounded-md bg-slate-200">
           <span className="mr-4 text-lg">{task}</span>
           <button className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"><FaArrowUp /></button>
           <button className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"><FaArrowDown /></button>
