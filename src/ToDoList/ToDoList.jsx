9c4fe7271152aee5373dbffb50286b8535511c30
sggyflt <sgflt88@gmail.com>
2024-09-04 23:01:09 +0200

    style: add basic styling

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -1,20 +1,23 @@
 import { useState } from "react";
+import { FaArrowUp } from "react-icons/fa";
+import { FaArrowDown } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 function ToDoList() {
 
   return(
-    <div>
-      <h1>To Do List</h1>
-      <div>
-        <input type="text" placeholder="Write a task..."/>
-        <button>Add</button>
+    <div className="flex flex-col items-start justify-start w-1/4 h-screen gap-6 m-auto mt-40">
+      <h1 className="text-5xl">To Do List</h1>
+      <div className="flex gap-1">
+        <input className="pl-2 border border-gray-300 rounded-md w-96" type="text" placeholder="Write a task..."/>
+        <button className="px-4 py-2 text-white bg-green-600 rounded-md hover:bg-green-700">Add</button>
       </div>
       <ol>
-        <li>
-          <span>Task 1</span>
-          <button>Delete</button>
-          <button>Move up</button>
-          <button>Move down</button>
+        <li className="flex items-center gap-1 px-4 py-2 rounded-md bg-slate-200">
+          <span className="mr-4 text-lg">Task 1</span>
+          <button className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"><FaArrowUp /></button>
+          <button className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"><FaArrowDown /></button>
+          <button className="px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-700"><MdDelete /></button>    
         </li>
       </ol>
     </div>
