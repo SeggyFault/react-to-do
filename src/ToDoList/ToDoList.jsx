22ac59baa454392327c778856f95576826334512
sggyflt <sgflt88@gmail.com>
2024-09-04 23:44:57 +0200

    feat: delete new task functionality

--- src/ToDoList/ToDoList.jsx
+++ src/ToDoList/ToDoList.jsx
@@ -20,6 +20,11 @@ function ToDoList() {
     }
   }
 
+  function deleteTask(index) {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  }
+
   return(
     <div className="flex flex-col items-start justify-start w-1/4 h-screen gap-6 m-auto mt-40">
       <h1 className="text-5xl">To Do List</h1>
@@ -33,7 +38,7 @@ function ToDoList() {
           <div className="flex gap-1">
             <button className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"><FaArrowUp /></button>
             <button className="px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700"><FaArrowDown /></button>
-            <button className="px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-700"><MdDelete /></button>  
+            <button className="px-4 py-2 text-white bg-red-600 rounded-md hover:bg-red-700" onClick={() => deleteTask(index)}><MdDelete /></button>  
           </div>    
         </li>)}
       </ol>
